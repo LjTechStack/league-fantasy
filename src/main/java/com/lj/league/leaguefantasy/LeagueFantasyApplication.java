ad9bff01b05160d3d49d926e14555e6136fec7d1
lj <ljtechstack@gmail.com>
2021-02-08 22:01:22 -0500

    init

---
+++ src/main/java/com/lj/league/leaguefantasy/LeagueFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.lj.league.leaguefantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueFantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lj/league/leaguefantasy/LeagueFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lj.league.leaguefantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
